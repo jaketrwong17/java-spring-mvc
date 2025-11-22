4b8c2cf4cde9093e022f404a323d30a2793a1220
Jake Truong <huyhieut2k5@gmail.com>
2025-11-23 00:17:20 +0700

    list bean

--- src/main/java/com/example/laptopshop/LaptopshopApplication.java
+++ src/main/java/com/example/laptopshop/LaptopshopApplication.java
@@ -2,12 +2,17 @@ package com.example.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopshopApplication.class, args);
+
+		ApplicationContext laptopshop = SpringApplication.run(LaptopshopApplication.class, args);
+		for (String s : laptopshop.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
 	}
 
 }
