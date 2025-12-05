3591a69b9620e5f0dae3efb0352205fdb73cd54f
Jake Truong <huyhieut2k5@gmail.com>
2025-12-05 08:56:01 +0700

    ggg

--- src/main/java/com/example/laptopshop/controller/UserController.java
+++ src/main/java/com/example/laptopshop/controller/UserController.java
@@ -6,21 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.Model;
 import com.example.laptopshop.domain.User;
+import com.example.laptopshop.repository.UserRepository;
 import com.example.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+
     }
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("hoidanit", test);
+
+        model.addAttribute("hoidanit", "test");
         model.addAttribute("hoidanit2", "Hello.......................");
         return "hello";
     }
@@ -33,7 +35,8 @@ public class UserController {
 
     @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-        System.out.println("run here" + hoidanit);
+        System.out.println("run here " + hoidanit);
+        this.userService.handleSaveUser(hoidanit);
         return "hello";
     }
 
--- src/main/java/com/example/laptopshop/service/UserService.java
+++ src/main/java/com/example/laptopshop/service/UserService.java
@@ -2,9 +2,24 @@ package com.example.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.laptopshop.domain.User;
+import com.example.laptopshop.repository.UserRepository;
+
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public String handleHello() {
         return "Hello from service";
+    }
+
+    public User handleSaveUser(User user) {
+        User eric = this.userRepository.save(user);
+        System.out.println(eric);
+        return eric;
     }
 }
